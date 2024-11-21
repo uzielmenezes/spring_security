925d494340888ae8bd0ab87c4784fb93ac280827
Uziel <uzielcivil@gmail.com>
2024-11-21 14:35:55 -0300

    chore: cors configuration

---
+++ src/main/java/tech/buildrun/spring_security/config/cors/CorsConfig.java
@@ -0,0 +1,20 @@
+package tech.buildrun.spring_security.config.cors;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
+
+
+
